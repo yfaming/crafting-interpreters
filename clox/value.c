6d904bcce66b5e8b21a82d942911fbbf509ec9cf
yfaming <yfaming@gmail.com>
2026-08-21 21:25:05 +0800

    chapter 18: Types of Values

--- clox/value.c
+++ clox/value.c
@@ -26,5 +26,23 @@ void freeValueArray(ValueArry* array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL: printf("nil"); break;
+        case VAL_NUMBER: printf("%g", AS_NUMBER(value));
+    }
+}
+
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) {
+        return false;
+    }
+    switch (a.type) {
+        case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NIL:    return true;
+        case VAL_NUMBER: return AS_NUMBER(a) == AS_BOOL(b);
+        default:         return false;  // Unreachable.
+    }
 }
--- clox/vm.c
+++ clox/vm.c
@@ -1,5 +1,7 @@
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <sys/_types/_va_list.h>
 
 #include "chunk.h"
 #include "common.h"
@@ -14,8 +16,45 @@ static void resetStack() {
     vm.stackTop = vm.stack;
 }
 
+static void runtimeError(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
 void initVM() {
     resetStack();
+}
+
+void freeVM() {
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+// `peek` returns a Value from the stack but doesn’t pop it.
+// `distance` is how far down from the top of the stack to look:
+// zero is the top, one is one slot down, etc.
+static Value peek(int distance) {
+    return vm.stackTop[-1-distance];
+}
+
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 }
 
 static InterpretResult run() {
@@ -24,11 +63,15 @@ static InterpretResult run() {
 // a - b 求值过程，先对 a 求值并入栈，然后对 b 求值并入栈。
 // 最后再执行减法操。此时 b 在栈顶，a 在下面。
 // 其他二元操作符同理。
-#define BINARY_OP(op) \
+#define BINARY_OP(valueType, op) \
     do { \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b); \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+            runtimeError("Operands must be numbers."); \
+            return INTERPRET_RUNTIME_ERROR; \
+        } \
+        double b = AS_NUMBER(pop()); \
+        double a = AS_NUMBER(pop()); \
+        push(valueType(a op b)); \
     } while (false)
 
     printf("\n== executing bytecode ==\n");
@@ -52,12 +95,31 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-
-            case OP_ADD:       BINARY_OP(+); break;
-            case OP_SUBSTRACT: BINARY_OP(-); break;
-            case OP_MULTIPLY:  BINARY_OP(*); break;
-            case OP_DIVIDE:    BINARY_OP(/); break;
-            case OP_NEGATE:    push(-pop()); break;
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE: push(BOOL_VAL(true)); break;
+            case OP_FALSE: push(BOOL_VAL(false)); break;;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:   BINARY_OP(BOOL_VAL,   >); break;
+            case OP_LESS:      BINARY_OP(BOOL_VAL,   <); break;
+            case OP_ADD:       BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBSTRACT: BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY:  BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:    BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
+            case OP_NEGATE:
+                if (!IS_NUMBER(peek(0))) {
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -71,18 +133,6 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-void freeVM() {
-}
-
-void push(Value value) {
-    *vm.stackTop = value;
-    vm.stackTop++;
-}
-
-Value pop() {
-    vm.stackTop--;
-    return *vm.stackTop;
-}
 
 InterpretResult interpret(const char* source) {
     Chunk chunk;
