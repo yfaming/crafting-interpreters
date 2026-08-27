d79eca7bc31a2d77c79c1eb83df02f278373fcdf
yfaming <yfaming@gmail.com>
2026-08-27 10:30:23 +0800

    chapter 22: Local Variables

--- clox/debug.c
+++ clox/debug.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 
 #include "debug.h"
@@ -24,6 +25,12 @@ static int simpleInstruction(const char* name, int offset) {
     return offset + 1;
 }
 
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t slot = chunk->code[offset + 1];
+    printf("%-16s %4d\n", name, slot);
+    return offset + 2;
+}
+
 // disassemble 当前 offset 处的指令，并返回下一条指令的 offset。
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
@@ -45,6 +52,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_FALSE", offset);
         case OP_POP:
             return simpleInstruction("OP_POP", offset);
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk, offset);
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk, offset);
         case OP_GET_GLOBAL:
             return constantInstruction("OP_GET_GLOBAL", chunk, offset);
         case OP_DEFINE_GLOBAL:
